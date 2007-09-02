24d1a04f2040a3c5095c275a688c69b98473b0dd
Shawn M Moore <sartak@gmail.com>
2007-09-01 22:14:35 -0500

    Initial import of Term::VT102::ZeroBased

---
+++ lib/Term/VT102/ZeroBased.pm
@@ -0,0 +1,83 @@
+#!perl
+package Term::VT102::ZeroBased;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+Term::VT102::ZeroBased - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Term::VT102::ZeroBased;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-term-vt102-zerobased at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Term-VT102-ZeroBased>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Term::VT102::ZeroBased
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Term-VT102-ZeroBased>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Term-VT102-ZeroBased>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Term-VT102-ZeroBased>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Term-VT102-ZeroBased>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
---
+++ t/001-basic.t
@@ -0,0 +1,8 @@
+#!perl -T
+use strict;
+use warnings;
+use Test::More tests => 1;
+use Term::VT102::ZeroBased;
+
+ok(1);
+
