312c2f16b64db09c32b31586a22e193e2ffec7f9
Shawn M Moore <sartak@gmail.com>
2009-06-04 23:25:07 -0400

    Use four space indent in the synopsis

--- lib/Term/VT102/ZeroBased.pm
+++ lib/Term/VT102/ZeroBased.pm
@@ -50,11 +50,11 @@ Term::VT102::ZeroBased - Term::VT102 but with zero-based indices
 
 =head1 SYNOPSIS
 
-  use Term::VT102::ZeroBased;
+    use Term::VT102::ZeroBased;
 
-  my $vt = Term::VT102::ZeroBased->new(cols => 80, rows => 24);
-  $vt->process("\e[H");                    # move to top left
-  printf "(%d, %d)!\n", $vt->x, $vt->y;    # (0, 0)!
+    my $vt = Term::VT102::ZeroBased->new(cols => 80, rows => 24);
+    $vt->process("\e[H");                    # move to top left
+    printf "(%d, %d)!\n", $vt->x, $vt->y;    # (0, 0)!
 
 =head1 DESCRIPTION
 
